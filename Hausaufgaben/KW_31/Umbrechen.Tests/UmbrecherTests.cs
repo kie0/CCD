11a04b7876b6c774dcb6a6e42863e479a8b6b436
soeren <soeren.kulick@cp.ag>
2016-08-04 09:58:38 +0200

    Aufgabe Umbrechen

---
+++ Hausaufgaben/KW_31/Umbrechen.Tests/UmbrecherTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Umbrechen;
+
+namespace Umbrechen.Tests
+{
+    [TestClass]
+    public class UmbrecherTests
+    {
+        [TestMethod]
+        public void Test_Umbrechen_9()
+        {
+            string text = "Es blaubt die Nacht,\ndie Sternlein blinken,\nSchneeflöcklein leis hernieder sinken.";
+            var cls = new Umbrecher();
+            var result = cls.Umbrechen(text, 9);
+            Assert.AreEqual("Es blaubt\ndie\nNacht,\ndie\nSternlein\nblinken,\nSchneeflö\ncklein\nleis\nhernieder\nsinken.", result);
+        }
+
+        [TestMethod]
+        public void Test_Umbrechen_14()
+        {
+            string text = "Es blaubt die Nacht,\ndie Sternlein blinken,\nSchneeflöcklein leis hernieder sinken.";
+            var cls = new Umbrecher();
+            var result = cls.Umbrechen(text, 14);
+            Assert.AreEqual("Es blaubt die\nNacht, die\nSternlein\nblinken,\nSchneeflöcklei\nn leis\nhernieder\nsinken.", result);
+        }
+
+        [TestMethod]
+        public void Test_WortLesen_Test1()
+        {
+            var cls = new Umbrecher();
+
+            var result = cls.WorteLesen("Wort1, Wort2 \nWort3.");
+            CollectionAssert.AreEqual(new [] { "Wort1,","Wort2","Wort3." }, result.ToArray());
+        }
+
+        [TestMethod]
+        public void Test_WorteNormieren_Test1()
+        {
+            var cls = new Umbrecher();
+
+            var result = cls.WorteNormieren(new[] { "Sternlein" }, 4);
+            CollectionAssert.AreEqual(new[] { "Ster", "nlei", "n" }, result.ToArray());
+        }
+
+        [TestMethod]
+        public void Test_WorteInZeilen_Test1()
+        {
+            var cls = new Umbrecher();
+
+            var result = cls.WorteInZeilen(new[] { "Ster", "nlei", "n", "so" }, 4);
+            CollectionAssert.AreEqual(new[] { "Ster", "nlei", "n so" }, result.ToArray());
+        }
+
+
+        [TestMethod]
+        public void Test_WorteInZeilen_Test2()
+        {
+            var cls = new Umbrecher();
+
+            var result = cls.WorteInZeilen(new[] { "Ster", "nlei", "n", "soso" }, 4);
+            CollectionAssert.AreEqual(new[] { "Ster", "nlei", "n","soso" }, result.ToArray());
+        }
+
+        [TestMethod]
+        public void Test_ZeilenInText_Test1()
+        {
+            var cls = new Umbrecher();
+
+            var result = cls.ZeilenInText(new[] { "Ster", "nlei", "n", "soso" });
+            Assert.AreEqual("Ster\nnlei\nn\nsoso", result);
+        }
+
+        [TestMethod]
+        public void Test_WortZusammenfügen_Test1()
+        {
+            var cls = new Umbrecher();
+
+            var result = cls.WorteZusammenfügen(new[] { "A,", "BB", "CCC." }, 3);
+            Assert.AreEqual("A,\nBB\nCCC\n." , result);
+        }
+
+        [TestMethod]
+        public void Test_WortZusammenfügen_Test2()
+        {
+            var cls = new Umbrecher();
+
+            var result = cls.WorteZusammenfügen(new[] { "A,", "BB", "CCC." }, 5);
+            Assert.AreEqual("A, BB\nCCC." , result);
+        }
+
+        [TestMethod]
+        public void Test_WortZusammenfügen_Test3()
+        {
+            var cls = new Umbrecher();
+
+            var result = cls.WorteZusammenfügen(new[] { "Sternlein", "so"}, 4);
+            Assert.AreEqual("Ster\nnlei\nn so", result);
+        }
+    }
+}
---
+++ Hausaufgaben/KW_31/Umbrechen/Umbrecher.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Umbrechen
+{
+    public class Umbrecher
+    {
+        public string Umbrechen(string text, int maxZeilenlänge)
+        {
+            var worte = WorteLesen(text);
+            var umbruchText = WorteZusammenfügen(worte, maxZeilenlänge);
+            return umbruchText;
+        }
+
+        public IEnumerable<string> WorteLesen(string text)
+        {
+            return text.Split(new[] {" ", "\n"}, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public IEnumerable<string> WorteNormieren(IEnumerable<string> worte, int maxZeichenlänge)
+        {
+            List<string> result = new List<string>();
+            foreach (var wort in worte)
+            {
+                if (wort.Length <= maxZeichenlänge) result.Add(wort);
+                else result.AddRange(TeileInNormierteWorte(maxZeichenlänge, wort));
+            }
+            return result;
+        }
+
+        private static IEnumerable<string> TeileInNormierteWorte(int maxZeichenlänge, string wort)
+        {
+            for (int i = 0; i < wort.Length; i += maxZeichenlänge)
+                yield return wort.Substring(i, Math.Min(maxZeichenlänge, wort.Length - i));
+        }
+
+        public IEnumerable<string> WorteInZeilen(IEnumerable<string> worte, int maxZeichenlänge)
+        {
+            string zeile = string.Empty;
+            foreach (var wort in worte)
+            {
+                if (zeile == "")
+                {
+                    zeile += wort;
+                }
+                else if (zeile.Length + 1 + wort.Length <= maxZeichenlänge)
+                {
+                    zeile += " " + wort;
+                }
+                else
+                {
+                    yield return zeile;
+                    zeile = wort;
+                }
+            }
+            if (!string.IsNullOrEmpty(zeile))
+                yield return zeile;
+        }
+
+        public string ZeilenInText(IEnumerable<string> worte)
+        {
+            return string.Join("\n", worte);
+        }
+
+        public string WorteZusammenfügen(IEnumerable<string> worte, int maxZeichenlänge)
+        {
+            var normiert = WorteNormieren(worte, maxZeichenlänge);
+            var umgebrochen = WorteInZeilen(normiert, maxZeichenlänge);
+            return ZeilenInText(umgebrochen);
+        }
+    }
+}
