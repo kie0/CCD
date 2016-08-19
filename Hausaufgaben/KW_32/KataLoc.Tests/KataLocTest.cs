3fb42693cc44b4582bdb8b0e023b0ac452f4a76c
soeren <soeren.kulick@cp.ag>
2016-08-19 07:52:32 +0200

    Kopiert von https://github.com/cpdanielg/CCD2/tree/master/Hausaufgaben/KW_32

---
+++ Hausaufgaben/KW_32/KataLoc.Tests/KataLocTest.cs
@@ -0,0 +1,95 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataLoc.Tests
+{
+    [TestClass]
+    public class KataLocTest
+    {
+        [TestMethod]
+        public void CountLOCTest()
+        {
+            var loc = new Loc();
+            Assert.AreEqual(6, loc.CountLOC("1\r\n\r\n// …\r\n2 // …\r\n/* … */\r\n/* … */ 3\r\n\r\n/*\r\n…\r\n*/\r\n4\r\n5 /* …\r\n… */ 6"));
+        }
+        
+        [TestMethod]
+        public void CountLoc_1()
+        {
+            var loc = new Loc();
+            Assert.AreEqual(1, loc.CountLOC("a = 0; /* Kommentar */"));
+        }
+
+        [TestMethod]
+        public void CountLoc_2()
+        {
+            var loc = new Loc();
+            Assert.AreEqual(1, loc.CountLOC("a = 0; /* Kommentar */ b = 2;"));
+        }
+
+        [TestMethod]
+        public void CountLoc_3()
+        {
+            var loc = new Loc();
+            Assert.AreEqual(1, loc.CountLOC("/* Kommentar1 */ var a = 2;  /* Kommentar2 */"));
+        }
+
+
+
+        [TestMethod]
+        public void RemoveCommentBlocksTest_0()
+        {
+            var loc = new Loc();
+            var lines = new[] { "var a = 2;  /* Ko", "12345", "ar*/ var g = 2;" };
+            CollectionAssert.AreEqual(new[] { "var a = 2;  /* Ko", "ar*/ var g = 2;" }, loc.RemoveCommentBlocks(lines).ToArray());
+        }
+
+        [TestMethod]
+        public void RemoveCommentBlocksTest_1()
+        {
+            var loc = new Loc();
+            var lines = new[] { "var a = 2;  /* Ko", "12345", "ar*/" };
+            CollectionAssert.AreEqual(new[] { "var a = 2;  /* Ko" }, loc.RemoveCommentBlocks(lines).ToArray());
+        }
+
+        [TestMethod]
+        public void RemoveCommentBlocksTest_2()
+        {
+            var loc = new Loc();
+            var lines = new[] { "/* Ko", "12345", "ar*/" };
+            CollectionAssert.AreEqual(new string[0], loc.RemoveCommentBlocks(lines).ToArray());
+        }
+
+        [TestMethod]
+        public void RemoveCommentBlocksTest_3()
+        {
+            var loc = new Loc();
+            var lines = new[] { "/* Ko", "12345", "ar*/ var g = 2;" };
+            CollectionAssert.AreEqual(new[] { "ar*/ var g = 2;" }, loc.RemoveCommentBlocks(lines).ToArray());
+        }
+
+        [TestMethod]
+        public void RemoveCommentBlocksTest_4()
+        {
+            var loc = new Loc();
+            var lines = new[] { " Kommentar */ var g = 2;" };
+            CollectionAssert.AreEqual(new[] { " Kommentar */ var g = 2;" }, loc.RemoveCommentBlocks(lines).ToArray());
+        }
+
+        [TestMethod]
+        public void RemoveCommentInLineTest_0()
+        {
+            var loc = new Loc();
+            Assert.AreEqual(" var g = 2; ", loc.RemoveCommentInLine("/* Kommentar */ var g = 2; /* Kommentar */"));
+        }
+
+        [TestMethod]
+        public void RemoveCommentInLineTest_1()
+        {
+            var loc = new Loc();
+            Assert.AreEqual(" var g = 2;  /* Teil Kommentar", loc.RemoveCommentInLine("/* Kommentar */ var g = 2; /* Kommentar */ /* Teil Kommentar"));
+        }
+
+    }
+}
+
---
+++ Hausaufgaben/KW_32/Loc/Loc.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KataLoc
+{
+    public class Loc
+    {
+        public int CountLOC(string csSource)
+        {
+            var lines = GetLines(csSource);
+
+            var removedCommentsInLines = RemoveCommentLines(lines);
+
+            var finalLines = RemoveCommentBlocks(removedCommentsInLines);
+
+            return finalLines.Count();
+        }
+
+        public IEnumerable<string> GetLines(string source)
+        {
+            return source.Split(new[] {"\r\n"}, StringSplitOptions.RemoveEmptyEntries);
+        }
+        
+        public IEnumerable<string> RemoveCommentLines(IEnumerable<string> lines)
+        {
+            return lines.Select(RemoveCommentInLine).Where(line => !line.StartsWith("//") && !string.IsNullOrWhiteSpace(line));
+        }
+
+        public string RemoveCommentInLine(string line)
+        {
+            var startComment = line.IndexOf("/*", StringComparison.InvariantCultureIgnoreCase);
+            var endComment = line.IndexOf("*/", StringComparison.InvariantCultureIgnoreCase);
+            if ((startComment < endComment) && (startComment != -1) && (endComment != -1))
+                return RemoveCommentInLine(line.Remove(startComment, endComment - startComment + 2));
+            return line;
+        }
+
+        public IEnumerable<string> RemoveCommentBlocks(IEnumerable<string> linesWithContent)
+        {
+            var inComment = false;
+
+            foreach (var line in linesWithContent)
+            {
+                if (inComment)
+                {
+                    int index = line.IndexOf("*/", StringComparison.InvariantCultureIgnoreCase);
+                    if (index >= 0)
+                    {
+                        if (index + 1 != line.Length - 1) // nicht letzte Zeichen
+                            yield return line;
+                        inComment = false;
+                    }
+                }
+                else
+                {
+                    int index = line.IndexOf("/*", StringComparison.InvariantCultureIgnoreCase);
+                    if (index >= 0)
+                    {
+                        if (index > 0) // nicht erstes Zeichen
+                            yield return line;
+                        inComment = true;
+                    }
+                    else
+                    {
+                        yield return line;
+                    }
+                }
+            }
+        }
+    }
+}
